a9063bfe1e884658143f693d44fb55fe7077e698
Connstance <connstance@gmail.com>
2012-04-28 11:55:59 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewApp::Application.config.secret_token = '065a8164c2d2297940986b12429237eec5e8159c260e2b119788a392dafe967acd0e58ee61b5244b413c01114b79edd2127c578a65ed8bad3533e3ee7a8c4383'
